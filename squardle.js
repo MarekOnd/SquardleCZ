2797dafeb1a51784c154353af73ef97536fd34e4
Trollak <ondrejtablet@gmail.com>
2022-07-07 17:55:50 +0200

    new words found summary, used letters

--- squardle.js
+++ squardle.js
@@ -5,21 +5,33 @@ let originDate = new Date("Thu Jun 30 2022 08:41:21 GMT+0200 (Central European S
 let indexOfSquardle;
   //let indexOfSquardle = Math.floor((date.getTime() - originDate.getTime())/(3600*24*1000));
 
+// information
+let numberOfSquardles = 7;
+
+
 // input data
 let squardleName = "";
 let size; // size of board
 let lettersInBoard = []; // letters in board
 let wordToFindPaths =[];
-let wordsToFindStrings = []
-let wordsFound = []; // bool array
+let wordsToFindStrings = [];
 
+
+let libraryName = "library_syn2015.json"
+let LIBRARY = [];
 // variables for finding word
 let mousePressed = false;
 let wordPath = {
     positions:[]
 }
 
+// progress
+let wordsFound = []; // bool array
+let bonusWordsFound = [];
+
 //design
+
+
 
 
 
@@ -27,17 +39,23 @@ async function initialize(){
     squardleName = "";
     lettersInBoard = [];
     wordToFindPaths =[];
-    wordsToFindStrings = []
+    wordsToFindStrings = [];
     wordsFound = [];
+    bonusWordsFound = [];
     await loadData();
+
+
+
     wordsToFindStrings = createWords(wordToFindPaths);
+
     window.addEventListener("pointerup",()=>{mouseUp()})
-    
+
 
     createBoard();
     initSelector();
     updateScore();
     updateFound();
+    updateActiveLettersInBoard()
 }
 // LOAD DATA
 async function loadData()
@@ -46,7 +64,6 @@ async function loadData()
     if(localStorage.getItem("index")!==null)
     {
         indexOfSquardle = JSON.parse(localStorage.getItem("index"))
-
     }
     else
     {
@@ -56,18 +73,18 @@ async function loadData()
 
     // loading structure
     let squardle = await getJson("./data/squardle_" + indexOfSquardle +".json");
-    let tmpBoard = squardle.letters;
+    lettersInBoard = JSON.parse(JSON.stringify(squardle.letters));
 
     // letters
         //let tmpBoard = await getJson("./data/Board" + indexOfSquardle +".json");
-    size = tmpBoard.length;
-    for (let i = 0; i < tmpBoard.length; i++) {
-        const element = tmpBoard[i];
-        for (let j = 0; j < element.length; j++) {
-            const subElement = element[j];
-            lettersInBoard.push(subElement);
-        }
-    }
+    // size = tmpBoard.length;
+    // for (let i = 0; i < tmpBoard.length; i++) {
+    //     const element = tmpBoard[i];
+    //     for (let j = 0; j < element.length; j++) {
+    //         const subElement = element[j];
+    //         lettersInBoard.push(subElement);
+    //     }
+    // }
 
     // words
     wordToFindPaths = squardle.wordsToFind;
@@ -84,8 +101,19 @@ async function loadData()
             const element = wordToFindPaths[i];
             wordsFound.push(false);
         }
-
     }
+
+    bonusWordsFound = [];
+    if(localStorage.getItem("bonus_" + indexOfSquardle) !== null)
+    {
+        
+        bonusWordsFound = JSON.parse(localStorage.getItem("bonus_" + indexOfSquardle));
+    }
+
+    // LIBRARY
+    LIBRARY = await getJson("./libraries/" + libraryName)
+    
+
 
 }
 
@@ -130,7 +158,7 @@ async function initSelector()
     }
     let i = 0;
     let squardle = await getJson("./data/squardle_" + i +".json");
-    while(squardle != false)
+    while(i < numberOfSquardles)
     {
         let newItem = document.createElement("option");
         newItem.textContent = squardle.name;
@@ -155,19 +183,19 @@ function createBoard()
         board.removeChild(board.firstChild)
     }
     // creates new board
-    for(var i = 0; i < size; i++)
+    for(var i = 0; i < lettersInBoard.length; i++)
     {
         let row = document.createElement("tr");
         row.className = "row";
 
-        for(var j = 0; j <size; j++)
+        for(var j = 0; j < lettersInBoard[0].length; j++)
         {
             let cell = document.createElement("td");
             cell.className = "cell";
             let button = document.createElement("button");
             button.className = "boardButton";
             
-            let text = document.createTextNode(lettersInBoard[i*size + j]);
+            let text = document.createTextNode(lettersInBoard[i][j]);
             let x = i;
             let y = j;
             
@@ -194,6 +222,37 @@ function getButton(x,y)
     return document.getElementsByClassName("row")[x].childNodes[y].firstChild;
 
 }
+
+function updateActiveLettersInBoard()
+{
+    let includedLetterPaths = []
+    for (let i = 0; i < wordToFindPaths.length; i++) {
+        if(!wordsFound[i])
+        {
+            includedLetterPaths.push(wordToFindPaths[i]);
+        }
+        
+    }
+    console.log(includedLetterPaths)
+    let allIncludedPositions = joinArrays(includedLetterPaths)
+    console.log(allIncludedPositions)
+    for (let i = 0; i < lettersInBoard.length; i++) {
+        
+        for (let y = 0; y < lettersInBoard.length; y++) {
+            
+            if(allIncludedPositions.filter(el=>(el[0]==i&&el[1]==y)) == 0)
+            {
+                let buttonToDeselect = getButton(i,y);
+                buttonToDeselect.classList.add("allWereFound")
+
+            }
+            
+        }
+        
+    }
+}
+
+
 
 // SELECTING WORD
 function mouseClick()
@@ -266,7 +325,7 @@ function createWord(path)
 {
     let string = "";
     path.positions.forEach(element => {
-        string += lettersInBoard[size*element[0] + element[1]];
+        string += lettersInBoard[element[0]][element[1]];
     });
     return string;
 }
@@ -304,11 +363,17 @@ function testMainWord() //<= goes here from mouseUp
             {
                 // new
                 wordsFound[i] = true;
+                updateScore();
+                updateFound();
+                updateActiveLettersInBoard()
+                return;
             }
         }
     }
-    updateScore();
-    updateFound();
+    if(LIBRARY.includes(wordPath))
+    {
+        bonusWordsFound.push([wordPath]);
+    }
 
 }
 
@@ -363,21 +428,57 @@ function updateFound()
     headerBox.appendChild(numText);
 
     // appends found words
+    let paragraph = document.createElement("div");
+
+    let words = []
     for (let i = 0; i < wordsFound.length; i++) {
         if(wordsFound[i])
         {
-            let word = createWord(wordToFindPaths[i]);
-            
-
-            let paragraph = document.createElement("p");
-            paragraph.textContent = word;
-            paragraph.classList.add("foundWord");
-            textBox.appendChild(paragraph);
+            console.log("push")
+            words.push(wordsToFindStrings[i])          
         }
     }
+
+
+    // THE ULTIMATE FUNCTION TO PRINT FOUND WORDS!!!
+    let currentLength = 4;
+    let stringOfSameLengthWords = ""
+    for (let i = 0; i < words.length; i++) {
+        if(words[i].length > currentLength)
+        {
+            fastAppendText(currentLength + " písmenná slova:", "foundWord-letterHeader", paragraph)
+            fastAppendText(stringOfSameLengthWords, "foundWord-words", paragraph)
+            fastAppendText(" + zbývá najít " + (howManyXLongWords(currentLength,wordsToFindStrings)-howManyXLongWords(currentLength,words)),"foundWord-missing",paragraph)
+            currentLength++;
+            while(words[i].length > currentLength)
+            {
+                fastAppendText(currentLength + " písmenná slova:", "foundWord-letterHeader", paragraph)
+                fastAppendText(" + zbývá najít " + (howManyXLongWords(currentLength,wordsToFindStrings)-howManyXLongWords(currentLength,words)),"foundWord-missing",paragraph)
+                currentLength++;
+            }
+            stringOfSameLengthWords = ""
+        }
+        stringOfSameLengthWords+= words[i] + "  ";
+    }
+    fastAppendText(currentLength + " písmenná slova:", "foundWord-letterHeader", paragraph)
+    fastAppendText(stringOfSameLengthWords, "foundWord-words", paragraph)
+    fastAppendText(" + zbývá najít " + howManyXLongWords(currentLength,wordsToFindStrings),"foundWord-missing",paragraph)
+    
+
+    paragraph.classList.add("foundWord");
+    textBox.appendChild(paragraph);
     saveProgress();
     
 }
+
+function sortWords(words)
+{
+    let newWords = JSON.parse(JSON.stringify(words));
+    newWords.sort()
+    newWords.sort((word)=>{word.length})
+    return newWords;
+}
+
 
 let foundWordsPopUp = false;
 function toggleFoundWordsPopUp()
@@ -398,6 +499,7 @@ function toggleFoundWordsPopUp()
 function saveProgress()
 {
     localStorage.setItem("progress_" + indexOfSquardle,JSON.stringify(wordsFound));
+    localStorage.setItem("bonus_" + indexOfSquardle, JSON.stringify(bonusWordsFound));
 }
 
 // CREATING LINE
@@ -417,7 +519,7 @@ function connectButtons(path)
     for (let i = 0; i < path.positions.length; i++) {
         const element = path.positions[i]
         let button = document.getElementsByClassName("row")[element[0]].childNodes[element[1]].getBoundingClientRect()
-        positions.push([button.left + button.width/2 - 7.5 + window.scrollX,button.top /*+ button.height/2*/ - 1 + window.scrollY])
+        positions.push([button.left + button.width/2 - 7.5 + window.scrollX,button.top /*+ button.height/2*/ -15 + window.scrollY])
     }
     return drawLine(positions)
 }
@@ -448,7 +550,8 @@ function createLineElement(x, y, length, angle) {
                + 'position: absolute; '
                + 'top: ' + y + 'px; '
                + 'left: ' + x + 'px; '
-               + 'border-radius: 10px';
+               + 'border-radius: 10px;'
+               + 'z-index: 2';
 
     line.setAttribute('style', styles);  
     return line;
@@ -471,6 +574,35 @@ function createLine(x1, y1, x2, y2) {
 }
 
 
+
+function fastAppendText(text,className,where)
+{
+    let textDiv =document.createElement("div")
+    textDiv.textContent = text;
+    textDiv.classList.add(className)
+    where.appendChild(textDiv);
+}
+
+function howManyXLongWords(x,words)
+{
+    return words.filter(el=>el.length == x).length;
+}
+
+function joinArrays(arrays)
+{
+    let megaArray = [];
+    for (let i = 0; i < arrays.length; i++) {
+        for (let y = 0; y < arrays[i].positions.length; y++) {
+            megaArray.push(arrays[i].positions[y])
+        }
+    }
+    console.log(megaArray)
+    return megaArray;
+}
+
+function includedInPositionArray(array, position)
+{
+}
 
 // calls starting program function
 // initialize()
