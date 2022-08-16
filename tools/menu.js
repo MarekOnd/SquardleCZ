c111b0398fb9124b0c90c7531b5154a475d1fa19
Trollak <ondrejtablet@gmail.com>
2022-08-16 14:30:25 +0200

    flexbox 1.0

--- tools/menu.js
+++ tools/menu.js
@@ -1,8 +1,12 @@
 function openTab(tabId)
 {
-    let editor = document.getElementById("squardleEditor");
-    let generator = document.getElementById("squardleGenerator");
-    editor.style.display = "none";
-    generator.style.display = "none"
-    document.getElementById(tabId).style.display = "block";
+    let tab = document.getElementById(tabId);
+    if(tab.style.display === "none")
+    {
+        tab.style.display = "block"
+    }
+    else
+    {
+        tab.style.display = "none"
+    }
 }
