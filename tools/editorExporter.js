364e82f82dd1fa7781f382af73b794fcf1616c8c
Pesopes <88830512+Pesopes@users.noreply.github.com>
2022-08-21 09:36:09 +0200

    import hints

--- tools/editorExporter.js
+++ tools/editorExporter.js
@@ -37,6 +37,16 @@ function applySquardleToExporter()
         document.getElementById("howManyMinutes").value = Math.floor(1.0*timeDelta/1000/60)%60;
     }
 
+    if(S.hints !== null){
+        document.getElementById("useCustomHints").checked = true
+        
+        document.getElementById("hintTimesStarting").value= S.hints.hintTimesIncluded
+        document.getElementById("hintTimesIncluded").value= S.hints.hintTimesIncluded
+        document.getElementById("hintRandomLetters").value= S.hints.hintRandomLetters
+    }else{
+        document.getElementById("howManyDays").checked = false
+    }
+
 }
 
 
