77cd41dc41739b4f51b3f9df59ee23ff17787a5e
Pesopes <88830512+Pesopes@users.noreply.github.com>
2022-11-01 18:10:07 +0100

    fixed caching problems maybe

--- scripts/updateHandler.js
+++ scripts/updateHandler.js
@@ -1,17 +1,18 @@
 function initUpdate()
 {
+    const version = 3
     if(localStorage.getItem("version") !== null)
     {
-        if(JSON.parse(localStorage.getItem("version")) !== 2)
+        if(JSON.parse(localStorage.getItem("version")) !== version)
         {
             localStorage.setItem("squardleSettings",JSON.stringify(defaultSettings));
-            localStorage.setItem("version",JSON.stringify(2))
+            localStorage.setItem("version",JSON.stringify(version))
         }
         
     }
     else// is null or undefined
     {
         localStorage.setItem("squardleSettings",JSON.stringify(defaultSettings));
-        localStorage.setItem("version",JSON.stringify(2))
+        localStorage.setItem("version",JSON.stringify(version))
     }
 }
